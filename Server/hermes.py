fa292a3afaab0cb870ef698b7b0fe4e69a3567e7
Victor Ribeiro Moura <victor@vicrmoura-ubuntu.(none)>
2014-10-16 02:41:21 -0300

    Simple threaded TCP server on localhost

--- Server/hermes.py
+++ Server/hermes.py
@@ -1,12 +1,29 @@
 #! /usr/bin/python
 
-from time import sleep
+import socket
+import threading
+import SocketServer
 
-i = 0
-with open("/hermes/logs/log", "a") as file:
-    file.write("====\n")
-    while True:
-        sleep(1)
-        file.write(str(i) + "\n")
-        file.flush()
-        i += 1
+class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
+
+    def handle(self):
+        data = self.request.recv(1024)
+        cur_thread = threading.current_thread()
+        response = "{}: {}".format(cur_thread.name, data)
+        self.request.sendall(response)
+
+class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    pass
+
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 9999
+
+    server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
+    ip, port = server.server_address
+
+    server_thread = threading.Thread(target=server.serve_forever)
+    server_thread.daemon = False
+    server_thread.start()
+    with open("/hermes/logs/log", "a") as log:
+        log.write("Server loop running in thread: {}\n".format(server_thread.name))
