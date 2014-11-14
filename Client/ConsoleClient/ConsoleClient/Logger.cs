9aaec402123e54b03b74156961c7f704255aab95
Filipe <luizmramos@gmail.com>
2014-11-14 19:28:32 -0200

    Adding logger class

---
+++ Client/ConsoleClient/ConsoleClient/Logger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Hermes
+{
+    static class Logger
+    {       
+        public static void log(string label, string text)
+        {
+            System.Diagnostics.Trace.WriteLine(string.Format("{0} >> [{1}] {2}", DateTime.Now.ToString("HH:mm:ss"), label, text));
+        }
+    }
+}
